eba17500e83c6b45f66c34b3d2924d06a2412f9c
lorist <dennis@pexip.com>
2024-03-19 07:43:09 +1100

    initial commit

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
-# python-docker
+# video watchdog
 
-A simple Python app for [Docker's Python Language Guide](https://docs.docker.com/language/python).
+A simple Python app that watches a folder for new files, uploads them to MediaCMS then deletes the local copy.
+
+## Installation
+
+1. Clone this repo
+2. Create a file in the watchdog folder called `.env`
+    In this file, add environment variables that the docker container will use to upload files to MediaCMS.
+    Example:
+        ```
+        MEDIACMS_USER='admin'
+        MEDIACMS_PASSWORD='password'
+        MEDIACMS_URL='https://mediacms.example.com/api/v1/media'
+        WATCH_FOLDER='../nginx-rtmp/videos'
+        ```
+3. Build and run the container: 
+        ```
+        docker compose build
+        docker compose up -d
+        ```
--- app.py
+++ app.py
@@ -1,6 +1,86 @@
-from flask import Flask
-app = Flask(__name__)
+import time
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+import os
+import requests
+import re
+import urllib3
+from dotenv import load_dotenv
 
-@app.route('/')
-def hello_world():
-    return 'Hello, Docker!'
+load_dotenv()
+
+# ignore ssl verify errors, cause they are annoying AF
+urllib3.disable_warnings()
+
+
+def upload(file):
+    auth = (os.getenv('MEDIACMS_USER'), os.getenv('MEDIACMS_PASSWORD'))
+    print('auth ', auth)
+    print('file: ', file)
+    x = re.findall("[^\.\/]+(?=\.)", file)
+    title = x[0]
+    description = ''
+    media_file = file
+    print('Uploading ', media_file)
+    try:
+        resp = requests.post(
+            url=os.getenv('MEDIACMS_URL'),
+            files={'media_file': open(media_file,'rb')},
+            data={'title': title, 'description': description},
+            auth=auth,
+            verify=False)
+        
+        if resp.ok:
+            try:
+                print('deleting file...')
+                os.remove(media_file)
+            except OSError:
+                pass
+
+    except requests.exceptions.HTTPError as err:
+        print(err)
+
+class Watcher:
+    # DIRECTORY_TO_WATCH = "../nginx-rtmp/videos"
+    DIRECTORY_TO_WATCH = os.getenv('WATCH_FOLDER')
+
+    def __init__(self):
+        self.observer = Observer()
+
+    def run(self):
+        event_handler = Handler()
+        self.observer.schedule(event_handler, self.DIRECTORY_TO_WATCH, recursive=True)
+        self.observer.start()
+        try:
+            while True:
+                time.sleep(5)
+        except:
+            self.observer.stop()
+            print("Error")
+
+        self.observer.join()
+
+
+class Handler(FileSystemEventHandler):
+
+    @staticmethod
+    def on_any_event(event):
+        if event.is_directory:
+            return None
+        
+        elif event.event_type == 'created':
+            # Taken any action here when a file is created.
+            print("Received modified event - %s." % event.src_path)
+            historicalSize = -1
+            while (historicalSize != os.path.getsize(event.src_path)):
+                historicalSize = os.path.getsize(event.src_path)
+                time.sleep(1)
+            print("Finished event - %s." % event.src_path)
+            print("uploading to mediacms - %s." % event.src_path)
+            upload(event.src_path)
+
+
+if __name__ == '__main__':
+    w = Watcher()
+    print('Watcher watching...')
+    w.run()
